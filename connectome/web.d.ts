70c02ad6e4478b2a1c5cd645ac05ce20db6f3ab0
countableclouds <aksharramkumar@gmail.com>
2021-10-23 12:48:10 -0700

    change simulation

---
+++ connectome/web.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function init_panic_hook(): void;
+/**
+*/
+export function main(): void;
---
+++ connectome/web.js
@@ -0,0 +1,3 @@
+import * as wasm from "./web_bg.wasm";
+export * from "./web_bg.js";
+wasm.__wbindgen_start();
